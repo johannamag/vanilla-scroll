d6baa95dc8c6ddc63cb98058b313526393e07ebc
johannamag <69107842+johannamag@users.noreply.github.com>
2020-11-10 15:14:48 +0100

    Added Fixed Nav

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//automatic update date in copyright
 const date = document.querySelector(".date");
 date.innerHTML = new Date().getFullYear();
+
+//NavContainer grows and shrinks with the number of links when toggled
 
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
@@ -12,5 +15,20 @@ navToggle.addEventListener("click", function () {
     linksContainer.style.height = `${linksHeight}px`;
   } else {
     linksContainer.style.height = 0;
+  }
+});
+
+//fixed navbar
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
   }
 });
