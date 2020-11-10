f039744eac38cdd57621a2462475e79903a36cfb
johannamag <69107842+johannamag@users.noreply.github.com>
2020-11-10 15:16:53 +0100

    Added Back to top button

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const date = document.querySelector(".date");
 date.innerHTML = new Date().getFullYear();
 
 //NavContainer grows and shrinks with the number of links when toggled
-
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
@@ -19,6 +18,7 @@ navToggle.addEventListener("click", function () {
 });
 
 //fixed navbar
+//back to top button
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
 
@@ -30,5 +30,11 @@ window.addEventListener("scroll", function () {
     navbar.classList.add("fixed-nav");
   } else {
     navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
   }
 });
